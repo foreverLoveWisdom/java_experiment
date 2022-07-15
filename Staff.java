0ad22b94af38f4f6aa995d9751b6aad137753d89
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2022-07-16 00:02:51 +0700

    Function overloading

---
+++ Staff.java
@@ -0,0 +1,30 @@
+public class Staff {
+  private String nameOfStaff;
+  private final int hourlyRate = 30;
+  private int hoursWorked;
+
+  public void printMessage() {
+    System.out.println("Calculating pay...");
+  }
+
+  public int calculatePay() {
+    printMessage();
+    int staffPay;
+    staffPay = hoursWorked * hourlyRate;
+
+    if (hoursWorked > 0) {
+      return staffPay;
+    } else {
+      return -1;
+    }
+  }
+
+  public int calculatePay(int bonus, int allowance) {
+    printMessage();
+    if (hoursWorked > 0) {
+      return hoursWorked * hourlyRate + bonus + allowance;
+    } else {
+      return 0;
+    }
+  }
+}
